389a0f2ed87b48267b1bd119d385dfd7f5440ef1
Om Preetham Bandi <86243960+OmPreetham@users.noreply.github.com>
2023-10-17 02:22:21 -0500

    finialized calculator project

--- beginner/10-calculator/main.py
+++ beginner/10-calculator/main.py
@@ -3,7 +3,7 @@ from art import logo
 print(logo)
 
 
-def calculator(operation, number_1, number_2):
+def arthemetic(operation, number_1, number_2):
     if operation == '+':
         return number_1 + number_2
     elif operation == '-':
@@ -17,16 +17,18 @@ def calculator(operation, number_1, number_2):
     else:
         print("Enter a valid operation")
 
-
-number_1 = float(input("Enter first number: "))
-while True:
-    operation = input("Enter operator \n '+'\n '-'\n '*'\n '/'\n '%'\n: ")
-    number_2 = float(input("What's next number "))
-    result = calculator(operation, number_1, number_2)
-    print(f"Result: {result}")
-    yn = input(f"Do you want to continue with {result} (y/n): ").lower()
-    if yn == 'y':
-        number_1 = result
-        continue
-    elif yn == 'n':
-        break
+def calculator():
+    number_1 = float(input("Enter first number: "))
+    while True:
+        operation = input("Enter operator \n '+'\n '-'\n '*'\n '/'\n '%'\n: ")
+        number_2 = float(input("What's next number "))
+        result = arthemetic(operation, number_1, number_2)
+        print(f"Result: {result}")
+        yn = input(f"Do you want to continue with {result} (y/n): ").lower()
+        if yn == 'y':
+            number_1 = result
+            continue
+        elif yn == 'n':
+            calculator()
+            
+calculator()
