ccb5ccfe10be81fb7392f8c72d9902f899f20c72
Om Preetham Bandi <86243960+OmPreetham@users.noreply.github.com>
2023-10-17 02:06:36 -0500

    added files via upload

---
+++ beginner/10-calculator/main.py
@@ -0,0 +1,32 @@
+from art import logo
+
+print(logo)
+
+
+def calculator(operation, number_1, number_2):
+    if operation == '+':
+        return number_1 + number_2
+    elif operation == '-':
+        return number_1 - number_2
+    elif operation == '/':
+        return number_1 / number_2
+    elif operation == '*':
+        return number_1 * number_2
+    elif operation == '%':
+        return number_1 % number_2
+    else:
+        print("Enter a valid operation")
+
+
+number_1 = float(input("Enter first number: "))
+while True:
+    operation = input("Enter operator \n '+'\n '-'\n '*'\n '/'\n '%'\n: ")
+    number_2 = float(input("What's next number "))
+    result = calculator(operation, number_1, number_2)
+    print(f"Result: {result}")
+    yn = input(f"Do you want to continue with {result} (y/n): ").lower()
+    if yn == 'y':
+        number_1 = result
+        continue
+    elif yn == 'n':
+        break
